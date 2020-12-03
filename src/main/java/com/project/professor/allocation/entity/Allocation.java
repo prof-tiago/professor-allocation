4a0ec649f0a42f641fe6a1f3a603d596f2155f8d
Tiago Santos <santos.t.j.dos@gmail.com>
2020-12-03 19:49:51 -0300

    Entity layer - Use Lombok

--- src/main/java/com/project/professor/allocation/entity/Allocation.java
+++ src/main/java/com/project/professor/allocation/entity/Allocation.java
@@ -1,9 +1,13 @@
 package com.project.professor.allocation.entity;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.DayOfWeek;
 import java.util.Date;
-import java.util.Objects;
 
+@NoArgsConstructor
+@Data
 public class Allocation {
 
     private Long id;
@@ -13,65 +17,4 @@ public class Allocation {
     private Date startHour;
 
     private Date endHour;
-
-    public Allocation() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public DayOfWeek getDayOfWeek() {
-        return dayOfWeek;
-    }
-
-    public void setDayOfWeek(DayOfWeek dayOfWeek) {
-        this.dayOfWeek = dayOfWeek;
-    }
-
-    public Date getStartHour() {
-        return startHour;
-    }
-
-    public void setStartHour(Date startHour) {
-        this.startHour = startHour;
-    }
-
-    public Date getEndHour() {
-        return endHour;
-    }
-
-    public void setEndHour(Date endHour) {
-        this.endHour = endHour;
-    }
-
-    @Override
-    public String toString() {
-        return "Allocation{" +
-                "id=" + id +
-                ", dayOfWeek=" + dayOfWeek +
-                ", startHour=" + startHour +
-                ", endHour=" + endHour +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Allocation that = (Allocation) o;
-        return Objects.equals(id, that.id) &&
-                dayOfWeek == that.dayOfWeek &&
-                Objects.equals(startHour, that.startHour) &&
-                Objects.equals(endHour, that.endHour);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, dayOfWeek, startHour, endHour);
-    }
 }
--- src/main/java/com/project/professor/allocation/entity/Professor.java
+++ src/main/java/com/project/professor/allocation/entity/Professor.java
@@ -1,7 +1,10 @@
 package com.project.professor.allocation.entity;
 
-import java.util.Objects;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
+@Data
 public class Professor {
 
     private Long id;
@@ -9,55 +12,4 @@ public class Professor {
     private String name;
 
     private String cpf;
-
-    public Professor() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    @Override
-    public String toString() {
-        return "Professor{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", cpf='" + cpf + '\'' +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Professor professor = (Professor) o;
-        return Objects.equals(id, professor.id) &&
-                Objects.equals(name, professor.name) &&
-                Objects.equals(cpf, professor.cpf);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, cpf);
-    }
 }
