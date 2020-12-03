d9a28e8a57a2d4e4dc2fd738a332109fa9319010
Tiago Santos <santos.t.j.dos@gmail.com>
2020-12-03 19:33:25 -0300

    Entity layer - Add constructor, getters, setters, toString, equals and hashCode

--- src/main/java/com/project/professor/allocation/entity/Allocation.java
+++ src/main/java/com/project/professor/allocation/entity/Allocation.java
@@ -2,6 +2,7 @@ package com.project.professor.allocation.entity;
 
 import java.time.DayOfWeek;
 import java.util.Date;
+import java.util.Objects;
 
 public class Allocation {
 
@@ -12,4 +13,65 @@ public class Allocation {
     private Date startHour;
 
     private Date endHour;
+
+    public Allocation() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public DayOfWeek getDayOfWeek() {
+        return dayOfWeek;
+    }
+
+    public void setDayOfWeek(DayOfWeek dayOfWeek) {
+        this.dayOfWeek = dayOfWeek;
+    }
+
+    public Date getStartHour() {
+        return startHour;
+    }
+
+    public void setStartHour(Date startHour) {
+        this.startHour = startHour;
+    }
+
+    public Date getEndHour() {
+        return endHour;
+    }
+
+    public void setEndHour(Date endHour) {
+        this.endHour = endHour;
+    }
+
+    @Override
+    public String toString() {
+        return "Allocation{" +
+                "id=" + id +
+                ", dayOfWeek=" + dayOfWeek +
+                ", startHour=" + startHour +
+                ", endHour=" + endHour +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Allocation that = (Allocation) o;
+        return Objects.equals(id, that.id) &&
+                dayOfWeek == that.dayOfWeek &&
+                Objects.equals(startHour, that.startHour) &&
+                Objects.equals(endHour, that.endHour);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, dayOfWeek, startHour, endHour);
+    }
 }
--- src/main/java/com/project/professor/allocation/entity/Course.java
+++ src/main/java/com/project/professor/allocation/entity/Course.java
@@ -1,8 +1,51 @@
 package com.project.professor.allocation.entity;
 
+import java.util.Objects;
+
 public class Course {
 
     private Long id;
 
     private String name;
+
+    public Course() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Course course = (Course) o;
+        return Objects.equals(id, course.id) &&
+                Objects.equals(name, course.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
 }
