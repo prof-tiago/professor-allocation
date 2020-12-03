fe839b81055334c0ad18b943783db29779ccd7b6
Tiago Santos <santos.t.j.dos@gmail.com>
2020-12-03 19:49:51 -0300

    Entity layer - Use Lombok

--- src/main/java/com/project/professor/allocation/entity/Department.java
+++ src/main/java/com/project/professor/allocation/entity/Department.java
@@ -1,51 +1,15 @@
 package com.project.professor.allocation.entity;
 
-import java.util.Objects;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Department {
 
     private Long id;
 
     private String name;
-
-    public Department() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public String toString() {
-        return "Department{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Department that = (Department) o;
-        return Objects.equals(id, that.id) &&
-                Objects.equals(name, that.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name);
-    }
 }
