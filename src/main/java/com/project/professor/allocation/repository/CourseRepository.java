c26fc25e3670ed252a475459ae386d834d101bfd
Tiago Santos <santos.t.j.dos@gmail.com>
2020-12-03 20:28:59 -0300

    Repository layer - Define extension methods

--- src/main/java/com/project/professor/allocation/repository/CourseRepository.java
+++ src/main/java/com/project/professor/allocation/repository/CourseRepository.java
@@ -4,7 +4,10 @@ import com.project.professor.allocation.entity.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {
 
+    List<Course> findByNameContainingIgnoreCase(String name);
 }
