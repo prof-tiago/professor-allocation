fd803701742bd44f4915bace3be79c412f4892f4
Tiago Santos <santos.t.j.dos@gmail.com>
2020-12-22 20:59:58 -0300

    Controller layer - Add base DTOs

---
+++ src/main/java/com/project/professor/allocation/dto/DepartmentBaseDTO.java
@@ -0,0 +1,13 @@
+package com.project.professor.allocation.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
+public class DepartmentBaseDTO {
+
+    private Long id;
+
+    private String name;
+}
