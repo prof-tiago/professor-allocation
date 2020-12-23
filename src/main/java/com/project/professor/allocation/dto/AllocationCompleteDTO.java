44b898f128cc96e66bbccae83d657fa2b88e2e39
Tiago Santos <santos.t.j.dos@gmail.com>
2020-12-22 21:07:39 -0300

    Controller layer - Add DTOs with the one to many relationships

---
+++ src/main/java/com/project/professor/allocation/dto/AllocationCompleteDTO.java
@@ -0,0 +1,11 @@
+package com.project.professor.allocation.dto;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class AllocationCompleteDTO extends AllocationSimpleDTO {
+}
