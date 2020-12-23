41db82aa09644c15291bd3bd868e59421bc96bcb
Tiago Santos <santos.t.j.dos@gmail.com>
2020-12-23 18:38:05 -0300

    Repository layer - Fix N + 1 query problem

--- src/main/java/com/project/professor/allocation/repository/AllocationRepository.java
+++ src/main/java/com/project/professor/allocation/repository/AllocationRepository.java
@@ -1,15 +1,27 @@
 package com.project.professor.allocation.repository;
 
 import com.project.professor.allocation.entity.Allocation;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface AllocationRepository extends JpaRepository<Allocation, Long> {
 
+    @EntityGraph(attributePaths = {"professor", "course", "professor.department"})
+    @Override
+    List<Allocation> findAll();
+
+    @EntityGraph(attributePaths = {"professor", "course", "professor.department"})
+    @Override
+    Optional<Allocation> findById(Long id);
+
+    @EntityGraph(attributePaths = {"professor", "course", "professor.department"})
     List<Allocation> findByProfessorId(Long professorId);
 
+    @EntityGraph(attributePaths = {"professor", "course", "professor.department"})
     List<Allocation> findByCourseId(Long courseId);
 }
--- src/main/java/com/project/professor/allocation/repository/ProfessorRepository.java
+++ src/main/java/com/project/professor/allocation/repository/ProfessorRepository.java
@@ -1,15 +1,27 @@
 package com.project.professor.allocation.repository;
 
 import com.project.professor.allocation.entity.Professor;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ProfessorRepository extends JpaRepository<Professor, Long> {
 
+    @EntityGraph(attributePaths = {"department"})
+    @Override
+    List<Professor> findAll();
+
+    @EntityGraph(attributePaths = {"department"})
+    @Override
+    Optional<Professor> findById(Long id);
+
+    @EntityGraph(attributePaths = {"department"})
     List<Professor> findByNameContainingIgnoreCase(String name);
 
+    @EntityGraph(attributePaths = {"department"})
     List<Professor> findByDepartmentId(Long departmentId);
 }
