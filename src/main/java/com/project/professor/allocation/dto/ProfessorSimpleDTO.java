75214641abc8a52668f9a7c4df6c3f51f5997e97
Tiago Santos <santos.t.j.dos@gmail.com>
2020-12-22 21:03:47 -0300

    Controller layer - Add DTOs with the many to one relationships

---
+++ src/main/java/com/project/professor/allocation/dto/ProfessorSimpleDTO.java
@@ -0,0 +1,13 @@
+package com.project.professor.allocation.dto;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class ProfessorSimpleDTO extends ProfessorBaseDTO {
+
+    private DepartmentBaseDTO department;
+}
