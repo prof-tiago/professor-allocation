b7b3bf43c35e0e7d396dda28adfdf514134a88e8
Tiago Santos <professor.tiago.dos.santos@gmail.com>
2021-06-05 23:15:45 -0300

    Test - Add integration tests to real database (for class)

---
+++ src/test/java/com/project/professor/allocation/service/ProfessorServiceIntegrationRealDBTest.java
@@ -0,0 +1,95 @@
+package com.project.professor.allocation.service;
+
+import com.project.professor.allocation.entity.Department;
+import com.project.professor.allocation.entity.Professor;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import java.util.List;
+
+@SpringBootTest
+@TestPropertySource(locations = "classpath:real-db-integration-tests.properties")
+public class ProfessorServiceIntegrationRealDBTest {
+
+    @Autowired
+    ProfessorService professorService;
+
+    @Test
+    public void findAll() {
+        // Arrange
+        String name = null;
+
+        // Act
+        List<Professor> professors = professorService.findAll(name);
+
+        // Print
+        professors.forEach(System.out::println);
+    }
+
+    @Test
+    public void findById() {
+        // Arrange
+        Long id = 1L;
+
+        // Act
+        Professor professor = professorService.findById(id);
+
+        // Print
+        System.out.println(professor);
+    }
+
+    @Test
+    public void save() {
+        // Arrange
+        Department department = new Department();
+        department.setId(1L);
+
+        Professor professor = new Professor();
+        professor.setId(null);
+        professor.setName("Professor 1");
+        professor.setCpf("111.111.111-11");
+        professor.setDepartment(department);
+
+        // Act
+        professor = professorService.save(professor);
+
+        // Print
+        System.out.println(professor);
+    }
+
+    @Test
+    public void update() {
+        // Arrange
+        Department department = new Department();
+        department.setId(1L);
+
+        Professor professor = new Professor();
+        professor.setId(1L);
+        professor.setName("Professor 2");
+        professor.setCpf("222.222.222-22");
+        professor.setDepartment(department);
+
+        // Act
+        professor = professorService.update(professor);
+
+        // Print
+        System.out.println(professor);
+    }
+
+    @Test
+    public void deleteById() {
+        // Arrange
+        Long id = 1L;
+
+        // Act
+        professorService.deleteById(id);
+    }
+
+    @Test
+    public void deleteAll() {
+        // Act
+        professorService.deleteAll();
+    }
+}
